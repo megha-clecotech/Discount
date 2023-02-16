13538ba3d5a5861a789e6493e0d42e2eb08d12ee
megha koshti <megha.koshti@clecotech.com>
2023-02-16 19:05:13 +0530

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
    resources :coupons
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root "coupons#index"
+  # root "coupons#index"
   # Defines the root path route ("/")
   # root "articles#index"
   
