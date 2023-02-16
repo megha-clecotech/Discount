025a84031962765c62cc5f769d819e0a9154120f
megha koshti <megha.koshti@clecotech.com>
2023-02-16 19:02:51 +0530

    added home page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ApplicationRecord
+
+  after_create :assign_default_coins
+ 
+ 
+  def assign_default_coins
+    self.update(coins: 0)
+  end
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
