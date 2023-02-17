45799253b91d6aed4f5323749cc42db98b88707a
megha koshti <megha.koshti@clecotech.com>
2023-02-17 12:00:36 +0530

    added asso

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,3 @@
 class Category < ApplicationRecord
+   has_many :coupons , through: :coupon_category_associations
 end
--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -1,4 +1,5 @@
 class Coupon < ApplicationRecord
   belongs_to :user
+  has_many :categories , through: :coupon_category_associations
   has_one_attached :coupon_file
 end
