4cb6fd1cc49b7c81a89d51bbd17d81f6d4b7e4bd
megha koshti <megha.koshti@clecotech.com>
2023-02-20 11:03:37 +0530

    Feedback model changes and like and dislike added

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,22 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import jQuery from "jquery";
+window.$ = window.jQuery = jQuery;
+ console.log($);
+
+$(function() {
+   $('[data-js-like-link]').on("click" ,function(event){
+      // if(like.clicked == true)
+      // {
+      //    $(this).css('color','red');
+      //    link.clicked = false;
+      // }
+      // else
+      // {
+      //    $(this).css('color','cyan');
+      // }
+      alert('You clicked the Hide link');
+     event.preventDefault();
+   });
+}) 
--- db/migrate/20230217120633_create_feedbacks.rb
+++ db/migrate/20230217120633_create_feedbacks.rb
@@ -1,9 +1,7 @@
 class CreateFeedbacks < ActiveRecord::Migration[7.0]
   def change
     create_table :feedbacks do |t|
-      t.integer :like
-      t.integer :dislike
-      t.string :comments
+      t.boolean :like
       t.references :user, null: false, foreign_key: true
       t.references :coupon, null: false, foreign_key: true
 
