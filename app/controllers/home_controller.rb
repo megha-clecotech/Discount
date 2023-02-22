626d88b14815100f99ed01415b35ad9fc995b88a
megha koshti <megha.koshti@clecotech.com>
2023-02-22 17:14:07 +0530

    category list

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,13 @@
 class HomeController < ApplicationController
   def index
     @coupons = Coupon.all
+    @categories  = Category.all
+    @arr = []
+    @categories.each do |c|
+      if c.subcategory.length != 0
+         @arr.push(c)
+      end
+    end
   end
   def profile
     @user = current_user
