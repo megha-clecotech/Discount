12c1ae9c72b7daf9a2cdcf453121c78fe48eddc0
megha koshti <megha.koshti@clecotech.com>
2023-02-23 16:38:49 +0530

    coupon category association deleted

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -1,7 +1,6 @@
 class Coupon < ApplicationRecord
   belongs_to :user
-  has_many :coupon_category_associations
-  has_many :categories , through: :coupon_category_associations
+  belongs_to :category
   has_many :feedbacks, dependent: :destroy
   has_one_attached :coupon_file
 end
---
+++ db/migrate/20230223105105_add_category_to_coupons.rb
@@ -0,0 +1,5 @@
+class AddCategoryToCoupons < ActiveRecord::Migration[7.0]
+  def change
+    add_reference :coupons, :category, foreign_key: true
+  end
+end
