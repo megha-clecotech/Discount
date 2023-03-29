13c65e59d51a416773e3d33d48b25f52fb54796a
megha koshti <megha.koshti@clecotech.com>
2023-03-29 13:58:50 +0530

    added working active admin with global variable

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -1,12 +1,15 @@
 class FeedbacksController < ApplicationController
-    before_action :set_coupon
-    def like
-        Feedback.create_like(@coupon, current_user)
-    end
-    def dislike
-        Feedback.create_dislike(@coupon, current_user)
-    end
-    def set_coupon
-        @coupon = Coupon.find(params[:id])
-    end 
+  before_action :set_coupon
+
+  def like
+    Feedback.create_like(@coupon, current_user)
+  end
+
+  def dislike
+    Feedback.create_dislike(@coupon, current_user)
+  end
+
+  def set_coupon
+    @coupon = Coupon.find(params[:id])
+  end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,5 @@
 class HomeController < ApplicationController
   def index
-    @coupons = Coupon.all
   end
 
   def about
